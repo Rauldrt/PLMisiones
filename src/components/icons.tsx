20e79a9f6a8834eb3055fecab0520d10c7575f23
raul duarte <rauldrt5@gmail.com>
2025-08-29 19:12:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -29,8 +29,9 @@ import {
   UsersRound,
   Link as LinkIcon,
 } from 'lucide-react';
+import type { SVGProps } from 'react';
 
-const Logo = (props: React.SVGProps<SVGSVGElement>) => (
+const Logo = (props: SVGProps<SVGSVGElement>) => (
     <svg width="32" height="32" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path d="M50 0L61.226 38.774L100 50L61.226 61.226L50 100L38.774 61.226L0 50L38.774 38.774L50 0Z" fill="currentColor" />
     </svg>
