38c828f6e4dfdf4cd373e09970b601743768a326
raul duarte <rauldrt5@gmail.com>
2025-08-30 00:39:50 +0000

    Runtime Error Server
    Error: Event handlers cannot be passed to Client

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -40,7 +42,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, onClick, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
 
     const createRipple = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -61,6 +63,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       }
 
       button.appendChild(circle);
+      
+      if(onClick) {
+        onClick(event);
+      }
     }
 
 
