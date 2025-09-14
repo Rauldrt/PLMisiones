c99cba40d9080faf2c53cd0d13197cb5439f8ad2
raul duarte <rauldrt5@gmail.com>
2025-09-14 20:39:40 +0000

    aun no se resulve el problema del despliegue en vercel, podria ser un pr

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -2,12 +2,12 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBX-buF-ifwTiMD64tOfQwi7PIw5IjnFyQ",
-  authDomain: "libertario-misiones-web.firebaseapp.com",
-  projectId: "libertario-misiones-web",
-  storageBucket: "libertario-misiones-web.appspot.com",
-  messagingSenderId: "1013928033094",
-  appId: "1:1013928033094:web:1420849b97f6b54a5b8347"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Use a function to safely initialize and get the app instance.
