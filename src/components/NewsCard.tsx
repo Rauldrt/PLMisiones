f8df7ee5ece1f1ec8bde23ed16efdff84ee25082
raul duarte <rauldrt5@gmail.com>
2025-09-19 03:10:00 +0000

    ahora la altura maxima esta bien, pero en el caso del video de youtube,

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -55,9 +55,16 @@ export function NewsCard({ article }: { article: NewsArticle }) {
         <Card className="flex w-full flex-col overflow-hidden bg-card border-border transition-transform hover:-translate-y-2">
             <CardHeader className="p-0">
                 <div className={cn(
-                    "relative w-full bg-muted overflow-hidden max-h-[500px]",
+                    "relative w-full bg-muted overflow-hidden",
                 )}>
-                    <Link href={`/noticias/${article.slug}`} className="block h-[500px] w-full" aria-label={article.title}>
+                    <Link 
+                      href={`/noticias/${article.slug}`} 
+                      className={cn(
+                        "block w-full",
+                        article.imageUrl && "h-[500px]" // Apply fixed height only for images
+                      )} 
+                      aria-label={article.title}
+                    >
                         {article.imageUrl ? (
                             <Image
                                 src={article.imageUrl}
@@ -71,7 +78,7 @@ export function NewsCard({ article }: { article: NewsArticle }) {
                             <div className="relative h-full w-full">
                                 <div className="pointer-events-none absolute inset-0 z-10" />
                                 <div 
-                                    className="h-full w-full [&_iframe]:!h-full [&_iframe]:!w-full [&_blockquote]:h-auto [&_blockquote]:w-full"
+                                    className="w-full [&_iframe]:!h-full [&_iframe]:!w-full [&_blockquote]:h-auto [&_blockquote]:w-full"
                                     dangerouslySetInnerHTML={{ __html: article.content }} 
                                 />
                             </div>
