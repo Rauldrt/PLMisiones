5d15237fb909a458d62b2b551ee4f0b15a7e0a3e
raul duarte <rauldrt5@gmail.com>
2025-09-20 01:49:50 +0000

    agregué un nuevo enlace de redes sociales pero no aparece

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -44,6 +44,21 @@ import {
 } from 'lucide-react';
 import type { SVGProps } from 'react';
 
+const TiktokIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M16.17 6.41a4.5 4.5 0 0 1-5.63.36 4.5 4.5 0 0 1-3.1-4.95V2h-3.44v10.37a6.5 6.5 0 0 0 6.5 6.51h.03a6.5 6.5 0 0 0 6.5-6.51V7.12a2.51 2.51 0 0 0-2.5-2.51h-.01a2.5 2.5 0 0 0-1.85 1.8Z" />
+    </svg>
+  );
+
 
 export const Icons = {
   News: Newspaper,
@@ -54,6 +69,7 @@ export const Icons = {
   Twitter,
   Instagram,
   Youtube,
+  Tiktok: TiktokIcon,
   Whatsapp: MessageCircle,
   ChevronLeft,
   ChevronRight,
